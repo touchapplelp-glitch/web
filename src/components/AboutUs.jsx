2d06240caa85581d21fcb761fcfb3e7ad1e5934c
Leandro <111095366+LeandroRapan@users.noreply.github.com>
2026-07-31 13:40:00 -0300

    Update AboutUs.jsx
    cambio de colores 2

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,10 +1,10 @@
 const AboutUs = () => {
     return (
       <div className="lg:pl-32 p-4 bg-gray-900 min-h-screen">
-        <h1 className="text-6xl font-bold text-red-700 mb-6">About Us</h1>
+        <h1 className="text-6xl font-bold text-purple-100 mb-6">About Us</h1>
         <h2 className="text-lg leading-relaxed text-gray-800 space-y-4">
           <p>
-            En <span className="font-semibold text-gray-100">Apple Touch</span> somos un equipo apasionado por la tecnología y comprometido con brindar una experiencia única a nuestros clientes. Desde nuestro inicio, nos hemos dedicado a acercarte los mejores productos Apple del mercado, garantizando calidad, transparencia y confianza en cada paso del camino.
+            En <span className="font-semibold text-gray-800">Apple Touch</span> somos un equipo apasionado por la tecnología y comprometido con brindar una experiencia única a nuestros clientes. Desde nuestro inicio, nos hemos dedicado a acercarte los mejores productos Apple del mercado, garantizando calidad, transparencia y confianza en cada paso del camino.
           </p>
           <p>
             Entendemos que comprar tecnología es más que una transacción: es una inversión y, muchas veces, una solución a tus necesidades. Por eso, nuestra prioridad no es solo ofrecerte los mejores dispositivos, sino también acompañarte antes, durante y después de tu compra.
@@ -16,7 +16,7 @@ const AboutUs = () => {
             En un mercado competitivo, nuestro valor diferencial está en nuestra transparencia. Te aseguramos productos originales, garantía real y una comunicación clara en todo momento. Queremos que sientas que podés confiar en nosotros y que estamos a tu lado para responder a cualquier inquietud.
           </p>
           <p>
-            En <span className="font-semibold text-red-700">Apple Touch</span>, no solo vendemos tecnología: construimos relaciones basadas en la honestidad y el respeto, buscando siempre lo mejor para nuestros clientes. ¡Gracias por elegirnos como tu lugar de confianza para todo lo relacionado con Apple!
+            En <span className="font-semibold text-purple-100">Apple Touch</span>, no solo vendemos tecnología: construimos relaciones basadas en la honestidad y el respeto, buscando siempre lo mejor para nuestros clientes. ¡Gracias por elegirnos como tu lugar de confianza para todo lo relacionado con Apple!
           </p>
         </h2>
       </div>
