eed0ec4bd42f8a0e2c6533c8147ccee5cb7d62fb
Leandro <111095366+LeandroRapan@users.noreply.github.com>
2026-08-05 14:40:09 -0300

    Update AdminPage.jsx

--- src/components/admin/AdminPage.jsx
+++ src/components/admin/AdminPage.jsx
@@ -54,7 +54,7 @@ return (
         <AdminLoginModal 
             isOpen={showLoginModal} 
             onClose={() => navigate("/")} 
-            onLoginSuccess={handleLoginSuccess} 
+           
         />
         
         {!showLoginModal && (
